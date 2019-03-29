7296135b562bb11d741c85e441e075c1d95a470a
Norwin Roosen <github@nroo.de>
2019-03-29 18:03:57 +0100

    add travis deploy task for fdroid #5
    pushes build result of platforms/android to `fdroid` branch,
    so fdroid can build using a standard gradle project.
    
    TODO: re-enable `on: tags` build option after testing

---
+++ tools/deploy-fdroid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# expects travis env vars: $repo, $tag, $rev, $GITHUB_TOKEN
+
+target=https://$GITHUB_TOKEN@github.com/sensebox/blockly-app.git
+branch=fdroid
+
+git clone --depth=1 -b $target $repo fdroid
+rm -rf fdroid/*
+mv -f platforms/android fdroid/
+cd fdroid
+git config user.name "Travis-CI"
+git config user.email "travis@travis-ci.org"
+git add -f --ignore-errors .
+git commit -m "android project for $tag at $rev" --amend # dont add new commits to avoid increasing repo size
+git push -f $target $branch > /dev/null 2>&1 # hide output to not leak GITHUB_TOKEN in logs
