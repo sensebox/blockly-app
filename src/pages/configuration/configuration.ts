a60691f5eeae7d25df1ca1e97db45d316599a4f3
Thiemann96 <e_thie10@uni-muenster.de>
2020-02-11 15:44:23 +0100

    added input fields for ids

--- src/pages/configuration/configuration.ts
+++ src/pages/configuration/configuration.ts
@@ -14,11 +14,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'configuration.html',
 })
 export class ConfigurationPage {
-
+  temp : string;
+  humi : string;
+  lux : string;
+  uv : string;
+  pm10 : string;
+  pm25 : string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   uploadStandardSketch(){
+    console.log(this.temp);
+    console.log(this.humi);
+    console.log(this.lux);
+    console.log(this.uv);
+    console.log(this.pm10);
+    console.log(this.pm25);
+    /**
+     * Start OTA Wizard here but with the pre defined sketch here and not with the blockly sketch 
+     * 
+     * */
   }
 
   ionViewDidLoad() {
